208f02e7f8d208998d98dc8899aa25ec939f53ef
vishnuvardhanreddy24 <vv284770@gmail.com>
2017-09-11 15:05:14 +0530

    Friend,job,user

--- src/test/java/com/niit/collabaration/collabarationBackend/BlogDAOTestCase.java
+++ src/test/java/com/niit/collabaration/collabarationBackend/BlogDAOTestCase.java
@@ -2,22 +2,51 @@ package com.niit.collabaration.collabarationBackend;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import com.collaborate.DAO.BlogDAO;
+import com.collaborate.model.Blog;
 
-public class BlogDAOTestCase{
+public class BlogDAOTest {
+
 	static BlogDAO blogDAO;
+	
 	@BeforeClass
 	public static void initialize()
 	{
-		AnnotationConfigApplicationContext annotationConfigApplicationConfig =new AnnotationConfigApplicationContext();
-		
-		annotationConfigApplicationConfig.scan("com.collaborate");
-		annotationConfigApplicationConfig.refresh();
+		AnnotationConfigApplicationContext annotationConfigAppContext=new AnnotationConfigApplicationContext();
+		annotationConfigAppContext.scan("com.collaborate");
+		annotationConfigAppContext.refresh();
+		blogDAO=(BlogDAO)annotationConfigAppContext.getBean("blogDAO");
+	
 	}
+	
 	@Test
-	public void test()
+	public void createBlogtest()
 	{
-		fail("not yet implemented");
+		Blog blog=new Blog();
+		blog.setBlogId(111);
+		blog.setBlogName("vishnu");
+		blog.setBlogContent("vishnuvardhan");
+		
+		blog.setCreateDate(new java.util.Date());
+		blog.setStatus("NA");
+		blog.setLikes(0);
+		assertTrue("Problem in Blog Creation",blogDAO.createBlog(blog));
+	
 	}
-
-}
+	
+	@Ignore
+	@Test
+	public void approveBlogTest()
+	{
+		Blog blog=new Blog();
+		blog.setBlogId(1001);
+		blog.setBlogName("vishnuvardhan");
+		blog.setBlogContent("vishnu");
+		
+		blog.setCreateDate(new java.util.Date());
+		blog.setStatus("NA");
+		blog.setLikes(0);
+		assertTrue("Problem in approving Blog",blogDAO.approvedBlog(blog));	
+	}}
+	
+	
+	
